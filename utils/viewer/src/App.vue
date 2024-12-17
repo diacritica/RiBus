90085b69c786c5bd9165766f6eeea7844eba0eee
AzazelN28 <asakon28@gmail.com>
2024-12-17 20:38:01 +0100

    feat: map

--- utils/viewer/src/App.vue
+++ utils/viewer/src/App.vue
@@ -1,10 +1,22 @@
 <script setup>
-import { VMap, VMapOsmTileLayer, VMapZoomControl } from 'vue-map-ui';
-import GeoJSON from './components/GeoJSON.vue';
+import { ref } from 'vue'
+import { VMap, VMapOsmTileLayer, VMapZoomControl } from 'vue-map-ui'
+import GeoJSON from './components/GeoJSON.vue'
+
+const center = ref({
+  lat: 40.35306192891293,
+  lng: -3.5252388696140207
+})
+const zoom = ref(14)
+
+function onViewChanged(e) {
+  // console.log(e)
+  // NOOP
+}
 </script>
 
 <template>
-  <VMap class="map" ref="containerComponent">
+  <VMap class="map" ref="containerComponent" :center="center" :zoom="zoom" @view-changed="onViewChanged">
     <VMapOsmTileLayer />
     <GeoJSON url="/data/malla_media_epsg4326.geojson" />
     <GeoJSON url="/data/estudiantes_epsg4326.geojson" />
